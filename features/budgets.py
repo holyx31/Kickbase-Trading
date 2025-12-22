2b0f2953389b758f2764e6d9b61bc33fc9f73991
holyx31 <dominik.meixner@live.de>
2025-12-22 22:39:15 +0100

    Update budgets.py

--- features/budgets.py
+++ features/budgets.py
@@ -1,15 +1,16 @@
-    from kickbase_api.user import get_budget, get_username
-    from kickbase_api.league import (
+from kickbase_api.user import get_budget, get_username
+from kickbase_api.league import (
     get_league_activities,
     get_league_ranking
-    )
-    from kickbase_api.manager import (
+)
+from kickbase_api.manager import (
     get_managers,
     get_manager_performance,
     get_manager_info,
-    )
-    from kickbase_api.others import get_achievement_reward
-    import pandas as pd
+)
+from kickbase_api.others import get_achievement_reward
+import pandas as pd
+
 
     def calc_manager_budgets(token, league_id, league_start_date, start_budget):
     """Calculate manager budgets based on activities, bonuses, and team performance."""
